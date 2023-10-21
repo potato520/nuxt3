8d9ccd5771cbae3eb9d86af577107f766e206fdb
potato520 <alexa456@163.com>
2023-10-21 13:41:58 +0800

    配置了ESLIN和preprent

--- app.vue
+++ app.vue
@@ -1,28 +1,26 @@
 <script setup lang="ts">
 // SEO 优化信息
 useSeoMeta({
-  // 网站标题
-  title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili',
-  // 网站描述
-  description:
-    'bilibili是国内知名的在线视频弹幕网站，拥有最棒的ACG氛围，哔哩哔哩内容丰富多元，涵盖动漫、电影、二次元舞蹈视频、在线音乐、娱乐时尚、科技生活、鬼畜视频等。下载客户端还可离线下载电影、动漫。',
-  // 搜索关键词
-  keywords: 'B站,bilibili,哔哩哔哩,哔哩哔哩动画,动漫,电影,在线动漫,高清电影',
+    // 网站标题
+    title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili',
+    // 网站描述
+    description:
+        'bilibili是国内知名的在线视频弹幕网站，拥有最棒的ACG氛围，哔哩哔哩内容丰富多元，涵盖动漫、电影、二次元舞蹈视频、在线音乐、娱乐时尚、科技生活、鬼畜视频等。下载客户端还可离线下载电影、动漫。',
+    // 搜索关键词
+    keywords: 'B站,bilibili,哔哩哔哩,哔哩哔哩动画,动漫,电影,在线动漫,高清电影',
 
-  // 绕开防盗链
-  referrer: 'no-referrer'
+    // 绕开防盗链
+    referrer: 'no-referrer'
 })
 </script>
 
-
 <template>
-  <!-- <NuxtLink to="/">首页</NuxtLink>
+    <!-- <NuxtLink to="/">首页</NuxtLink>
   <NuxtLink to="/video">视频</NuxtLink> -->
 
-  <!-- 缓存10级页面，方便用户切换 -->
-  <NuxtPage :keepalive="{max:10}"/>
+    <!-- 缓存10级页面，方便用户切换 -->
+    <NuxtPage :keepalive="{ max: 10 }" />
 </template>
-
 
 <style lang="scss">
 // 基础样式
@@ -32,8 +30,8 @@ useSeoMeta({
 
 /* vant-ui 主题定制 */
 :root {
-  --van-primary-color: #fb7299 !important;
-  --van-back-top-background: #fbfbfb !important;
-  --van-back-top-text-color: #666 !important;
+    --van-primary-color: #fb7299 !important;
+    --van-back-top-background: #fbfbfb !important;
+    --van-back-top-text-color: #666 !important;
 }
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,61 +1,12 @@
-<script setup lang="ts">
-import type { VideoItem } from '@/types/video';
-
-
-const { data: channelList } = await useFetch('/api/channel')
-// console.log(channelList);
-// 获取视频列表数据
-const { data: videoList } = await useFetch('/api/video')
-
-
-const list = ref<VideoItem[]>([]);
-const loading = ref(false);
-const finished = ref(false);
-
-let page = 1
-let pageSize = 20
-const onLoad = () => {
-    // 正在加载
-    loading.value = false
-    // console.log('触底了');
-    // 根据页码提取数据
-    const data = videoList.value?.slice((page - 1) * pageSize, page * pageSize) as VideoItem[]
-    // 最佳到页面数组中
-    list.value.push(...data)
-    page++
-    if (videoList.value?.length === list.value.length) {
-        finished.value = true
-    }
-}
-
-// 初始化，主动请求前20条数据,方便SEO
-onLoad()
-</script>
+<script setup lang="ts"></script>
 <template>
-
-    <!-- 公共头部 -->
-    <AppHeader />
-    <!-- 频道模块 -->
-    <van-tabs>
-        <van-tab v-for="item in channelList" :key="item.id" :title="item.name" />
-    </van-tabs>
-
-    <!-- 内容视频区域 -->
-    <van-list v-model:loading="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-        <!-- <van-cell v-for="item in list" :key="item" :title="item" /> -->
-        <div class="video-list">
-            <AppVideo v-for="item in list" :key="item.bvid" :item="item"/>
-        </div>
-    </van-list>
+    <div class="container">
+        <div class="countries" />
+    </div>
 </template>
 <style lang="scss">
-
-// 视频列表
-.video-list {
-    display: flex;
-    flex-wrap: wrap;
-    padding: 10px 5px;
+.container {
+    .countries {
+    }
 }
-
-
 </style>
